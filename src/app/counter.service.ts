60a9eb86c4dc0bd512eaa3dc02e933dd9cff4dda
Rocio Sirvent <rocio.sirvent@finologee.com>
2020-05-21 18:16:33 +0200

    use the UsersService, configure and use the CounterService

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -5,5 +5,18 @@ import { Injectable } from '@angular/core';
 })
 export class CounterService {
 
+  activeToInactiveCounter = 0;
+  inactiveToActiveCounter = 0;
+
+  incrementActiveToInactiveList() {
+    this.activeToInactiveCounter++;
+    console.log('Number of times an inactive user has changed to active: ' + this.activeToInactiveCounter);
+  }
+
+  incrementInactiveToActiveList() {
+    this.inactiveToActiveCounter++;
+    console.log('Number of times an active user has changed to inactive: ' + this.inactiveToActiveCounter);
+  }
+
   constructor() { }
 }
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { CounterService } from './counter.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,12 +12,14 @@ export class UsersService {
   setToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]); // add to active list
     this.inactiveUsers.splice(id, 1); // remove from inactive list
+    this.counterService.incrementActiveToInactiveList();
   }
 
   setToInactive(id: number) {
     this.inactiveUsers.push(this.activeUsers[id]); // add to inactive list
     this.activeUsers.splice(id, 1); // remove from active list
+    this.counterService.incrementInactiveToActiveList();
   }
 
-  constructor() { }
+  constructor(private counterService: CounterService) { }
 }
